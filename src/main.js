c4b138054d77662536e1dba7c8a3ee31d4082d86
Tarrence van As <tarrence13@gmail.com>
2016-01-17 12:17:53 -0800

    add request id's

--- src/main.js
+++ src/main.js
@@ -1,22 +1,27 @@
 import { create } from 'axios';
 
-const CLIENT = Symbol();
 const BASE_CONF = Symbol();
+const CLIENT = Symbol();
+const REQUEST_INCREMENTER = Symbol();
 
 export default class Eth {
     constructor(providerUrl) {
         this[BASE_CONF] = {
             jsonrpc: '2.0',
         };
+
         this[CLIENT] = create({
             baseURL: providerUrl,
             timeout: 1000,
             headers: { 'Content-Type': 'application/json' },
         });
+
+        this[REQUEST_INCREMENTER] = 0;
     }
 
     send(method, params) {
         return this[CLIENT].post('', {
+            id: this[REQUEST_INCREMENTER]++,
             ...this[BASE_CONF],
             ...params,
             method,
